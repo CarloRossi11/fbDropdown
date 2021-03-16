274a8ca57dbc92acedd6b6015449621429dff915
Carlo Berardelli <carloberardelli@yahoo.com>
2021-03-16 18:28:12 -0400

    created dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar>
+      <NavItem icon="😀" />
+      <NavItem icon="😀" />
+      <NavItem icon="😀" />
+      <NavItem icon="Drop">
+        <DropdownMenu/>
+      </NavItem>
+      
+    </Navbar>
   );
 }
 
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return(
+    <a href="#" className="menu-item">
+      <span className ="icon-button">{props.leftIcon}</span>
+      {props.children}
+      <span className ="icon-right">{props.rightIcon}</span>
+    </a>
+    )
+  }
+
+  return(
+    <div className="dropdown">
+      <DropdownItem leftIcon="MP">My Profile</DropdownItem>
+      <DropdownItem leftIcon="S">
+          Settings
+      </DropdownItem>
+    </div>
+
+  )
+}
+
+function Navbar(props) {
+  return(
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        { props.children }
+      </ul>
+    </nav>
+  )
+}
+
+function NavItem(props) {
+
+  const [open, setOpen] = useState(false)
+
+  return(
+  <li className="nav-item">
+    <a href="#" className="icon-button" onClick={() => {setOpen(!open)}}>
+      {props.icon}
+    </a>
+    {open && props.children}
+  </li>
+  )
+}
 export default App;
